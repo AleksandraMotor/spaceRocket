5e116ff26e29be283928185d79c89a53ea7d235e
ppolczyk <polczyk.patryk91@gmail.com>
2021-06-01 00:35:09 +0200

    first working class

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,116 @@
 //PROPERTIES
 class Speed
 {
-  Public:
+  public:
     int v;
 };
 
 class NoSeats       //number of seats
 {
-  Public:
+  public:
     int n;
 };
 
 class Capacity
 {
-  Public:
+  public:
     int cap;
 };
 
 class Tank
 {
-  Public:
+  public:
     int vol;
 };
 
 class Firepower
 {
-  Public:
+  public:
     int energy;   //to trzeba zmienic, nie podoba mi sie ta nazwa
 };
 
-class Vehicle_Builder
+class Vehicle
 {
-  Public:
-    virtual Speed* getSpeed() = 0;
-    virtual NoSeats* getNoSeats = 0;
-    virtual Capacity* getCapacity = 0;
+  public:
+    Speed*    velocity;
+    NoSeats*  space;
+    Capacity* hold;
+
+    void parameters()
+    {
+      std::cout << "Maximum speed: " << velocity->v << std::endl;
+      std::cout << "Maximum stuff: " << space->n << std::endl;
+      std::cout << "Maximum hold capacity: " <<  hold->cap << std::endl;
+    }
 };
 
-
-/*class Bullet_Builder
-{
-} 
-*/
-
-//---------Concretes builders-------------
-/*class Moon_Rocket
+/*class Bullet
 {
 }
 */
+
+class VehicleBuilder
+{
+  public:
+    virtual Speed* getSpeed() = 0;
+    virtual NoSeats* getNoSeats() = 0;
+    virtual Capacity* getCapacity() = 0;
+};
+
+/*class BulletBuilder
+{
+} 
+*/
+class DesignDirector     //Werner Von Braun !!
+{
+  VehicleBuilder* vehicleBuilder;
+
+  public:
+    void setVehicleBuilder(VehicleBuilder* newVehicleBuilder)
+    {
+      vehicleBuilder = newVehicleBuilder;
+    }
+
+    Vehicle* getVehicle()
+    {
+      Vehicle* vehicle = new Vehicle();
+
+      vehicle->velocity = vehicleBuilder->getSpeed();
+
+      vehicle->space = vehicleBuilder->getNoSeats();
+
+      vehicle->hold = vehicleBuilder->getCapacity();
+
+      return vehicle;
+    }
+};
+//---------Concretes builders-------------
+class MoonRocket : public VehicleBuilder
+{
+    public:
+      Speed* getSpeed()
+      {
+        Speed* velocity = new Speed();
+        velocity->v = 30000;
+        return velocity;
+      } 
+
+      NoSeats* getNoSeats()
+      {
+        NoSeats* space = new NoSeats();
+        space->n = 6;
+        return space;
+      }
+
+      Capacity* getCapacity()
+      {
+        Capacity* hold = new Capacity();
+        hold->cap = 50;
+        return hold;
+      }
+
+};
+
 
 /*class Mars_Rocket
 {
@@ -76,10 +139,7 @@ class Vehicle_Builder
 */
 //----------------------------------------
 
-/*class Design_Director     //Werner Von Braun !!
-{
-}
- */
+
 
 /*class Messeges            //tutaj mysle dac powitalna wiadomosc oraz wiadomosci o tym z czego sklada sie dana rakieta jako metody klasy 
 {                             
@@ -89,8 +149,15 @@ class Vehicle_Builder
 
 int main()
 {
+  Vehicle* vehicle;
 
+  DesignDirector director;
 
+  MoonRocket moonRocket;
 
+  std::cout << "Moon Rocket" << std::endl;
+  director.setVehicleBuilder(&moonRocket);
+  vehicle = director.getVehicle();
+  vehicle->parameters();
   return 0;
 }
