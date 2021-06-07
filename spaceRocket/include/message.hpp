123a091edce75a46a5109b7ce343c55573fa12e3
ppolczyk <polczyk.patryk91@gmail.com>
2021-06-07 17:22:40 +0200

    headers files

---
+++ spaceRocket/include/message.hpp
@@ -0,0 +1,14 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+class Messages            
+{
+  public:
+  void welcome();
+  void goodbye();
+  void error();
+  void ask();
+  void assortment();
+}; 
+
+#endif
